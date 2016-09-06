95aa125e6ad756100c795badeb8826bd5d5604ee
sassoukc <kechaou.sonia@yahoo.fr>
2016-09-06 15:34:54 +0100

    Create mapping.sh

---
+++ mapping.sh
@@ -0,0 +1,4 @@
+tophat -o SRR1813874 -p 6 --no-coverage-search ../test/index2/hgindex2 SRR1813874_1.fastq SRR1813874_2.fastq ../test/index2/hgindex2.fa &
+tophat -o SRR1813872 -p 6 --no-coverage-search ../test/index2/hgindex2 SRR1813872_1.fastq SRR1813872_2.fastq ../test/index2/hgindex2.fa &
+tophat -o SRR1813892 -p 6 --no-coverage-search ../test/index2/hgindex2 SRR1813892_1.fastq SRR1813892_2.fastq ../test/index2/hgindex2.fa &
+tophat -o SRR1813894 -p 6 --no-coverage-search ../test/index2/hgindex2 SRR1813894_1.fastq SRR1813894_2.fastq ../test/index2/hgindex2.fa &
