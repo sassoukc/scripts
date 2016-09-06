6f81ee8cca196fdc33cc3f00601654768e2f8cf0
sassoukc <kechaou.sonia@yahoo.fr>
2016-09-06 15:32:15 +0100

    comptage.sh

---
+++ comptage.sh
@@ -0,0 +1,4 @@
+htseq-count -f bam -t gene  -i gene_id SRR1813872/accepted_hits.bam hh.gtf > SRR1813872.count &
+htseq-count -f bam -t gene  -i gene_id SRR1813874/accepted_hits.bam hh.gtf > SRR1813874.count &
+htseq-count -f bam -t gene  -i gene_id SRR1813892/accepted_hits.bam hh.gtf > SRR1813892.count &
+htseq-count -f bam -t gene  -i gene_id SRR1813894/accepted_hits.bam hh.gtf > SRR1813894.count &
